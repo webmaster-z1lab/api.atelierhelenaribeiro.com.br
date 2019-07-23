5bfb13a99d4655afaf2d936a752fc3930dafd860
Felipe da Silva Pinheiro <felipe.spinheiro88@gmail.com>
2019-07-23 14:50:16 -0300

    Remove front-end

--- resources/views/products/show.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.default')
-
-@section('title', 'Detalhes do produto')
-
-@section('content')
-
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,3 @@
 <?php
 
-Route::get('/', 'HomeController@index')->name('home');
-
 Auth::routes();
-
-Route::get('/products', function () {
-    return view('products.index');
-})->name('products.index');
-
-Route::get('/products/show', function () {
-    return view('products.show');
-})->name('products.show');
-
-Route::get('/products/save', function () {
-    return view('products.save');
-})->name('products.save');
