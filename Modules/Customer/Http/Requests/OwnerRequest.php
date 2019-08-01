2a221cae5581106ac8a9534ad594c50f631f84bc
Felipe da Silva Pinheiro <felipe.spinheiro88@gmail.com>
2019-08-01 11:49:14 -0300

    Fixed owner requests

--- Modules/Customer/Http/Requests/OwnerRequest.php
+++ Modules/Customer/Http/Requests/OwnerRequest.php
@@ -2,9 +2,9 @@
 
 namespace Modules\Customer\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\ApiFormRequest;
 
-class OwnerRequest extends FormRequest
+class OwnerRequest extends ApiFormRequest
 {
     /**
      * Get the validation rules that apply to the request.
@@ -14,7 +14,11 @@ class OwnerRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'       => 'bail|required|string|min:3',
+            'document'   => 'bail|required|cpf',
+            'email'      => 'bail|required|email',
+            'birth_date' => 'bail|required|date|before_or_equal:today',
+            $this->getPhonesRules(),
         ];
     }
 
@@ -25,6 +29,41 @@ class OwnerRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return TRUE;
+    }
+
+    public function attributes()
+    {
+        return [
+            'name'       => 'nome',
+            'document'   => 'CNPJ',
+            'email'      => 'e-mail',
+            'birth_date' => 'data de nascimento',
+            $this->getPhonesAttributes(),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    protected function getPhonesRules(): array
+    {
+        return [
+            'phones'               => 'bail|required|array|min:1',
+            'phones.*.phone'       => 'bail|required|cell_phone',
+            'phones.*.is_whatsapp' => 'bail|required|bool_custom',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    protected function getPhonesAttributes(): array
+    {
+        return [
+            'phones'               => 'telefones',
+            'phones.*.phone'       => 'telefone',
+            'phones.*.is_whatsapp' => 'whatsapp',
+        ];
     }
 }
--- Modules/Customer/Http/Requests/OwnerUpdateRequest.php
+++ Modules/Customer/Http/Requests/OwnerUpdateRequest.php
@@ -2,29 +2,17 @@
 
 namespace Modules\Customer\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-
-class OwnerUpdateRequest extends FormRequest
+class OwnerUpdateRequest extends OwnerRequest
 {
     /**
-     * Get the validation rules that apply to the request.
-     *
      * @return array
      */
-    public function rules()
+    protected function getPhonesRules(): array
     {
         return [
-            //
+            'phones'               => 'bail|nullable|array',
+            'phones.*.phone'       => 'bail|required|cell_phone',
+            'phones.*.is_whatsapp' => 'bail|required|bool_custom',
         ];
-    }
-
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
     }
 }
