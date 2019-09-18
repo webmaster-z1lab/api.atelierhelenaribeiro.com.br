3aa77eb58b0b39bbd21103f2517e9eb3dd7ee7a0
Alisson Roberto <alissoniecp@gmail.com>
2019-09-18 11:18:29 -0300

    Bug fix in product & template thumbnail;

--- Modules/Stock/Http/Resources/BasicProductResource.php
+++ Modules/Stock/Http/Resources/BasicProductResource.php
@@ -2,10 +2,13 @@
 
 namespace Modules\Stock\Http\Resources;
 
+use App\Traits\FileUrl;
 use Illuminate\Http\Resources\Json\Resource;
 
 class BasicProductResource extends Resource
 {
+    use FileUrl;
+
     /**
      * Transform the resource into an array.
      *
@@ -20,7 +23,7 @@ class BasicProductResource extends Resource
         return [
             'id'          => (string) $this->resource->_id,
             'barcode'     => $this->resource->barcode,
-            'thumbnail'   => $this->resource->thumbnail_url,
+            'thumbnail'   => $this->getFileUrl(),
             'size'        => $this->resource->size,
             'color'       => $this->resource->color,
             'template_id' => $this->resource->template_id,
@@ -28,5 +31,15 @@ class BasicProductResource extends Resource
             'created_at'  => $this->resource->created_at->toDateTime()->format(\DateTime::W3C),
             'updated_at'  => $this->resource->updated_at->toDateTime()->format(\DateTime::W3C),
         ];
+    }
+
+    /**
+     * @return \Illuminate\Config\Repository|mixed|string
+     */
+    private function getFileUrl(): string
+    {
+        return (isset($this->resource->thumbnail))
+            ? $this->fileUrl($this->resource->thumbnail)
+            : config('image.sizes.thumbnail.placeholder');
     }
 }
